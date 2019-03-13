df67d7ddbb31531b697c4dfd8ac2ad8215cf9f56
FabioSSena <fabiossena@gmail.com>
2019-03-13 01:18:51 -0300

    Ajuste acento sábado

--- src/main/java/com/packageIxia/sistemaControleEscala/models/referencias/DiaSemanaEnum.java
+++ src/main/java/com/packageIxia/sistemaControleEscala/models/referencias/DiaSemanaEnum.java
@@ -2,7 +2,7 @@ package com.packageIxia.sistemaControleEscala.models.referencias;
 
 public enum DiaSemanaEnum {
 
-	segunda(1, "Segunda-feira"), terca(2, "Terça-feira"), quarta(3, "Quarta-feira"), quinta(4, "Quinta-feira"), sexta(5, "Sexta-feira"), sabado(6, "Sabádo"), domingo(7, "Domingo");
+	segunda(1, "Segunda-feira"), terca(2, "Terça-feira"), quarta(3, "Quarta-feira"), quinta(4, "Quinta-feira"), sexta(5, "Sexta-feira"), sabado(6, "Sábado"), domingo(7, "Domingo");
 
     public DiaSemana diaSemana;
     
