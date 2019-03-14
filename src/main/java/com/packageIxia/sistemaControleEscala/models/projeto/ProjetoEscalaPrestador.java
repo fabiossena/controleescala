e0ffb3e410595a7f839b7b80e70909d978624a48
FabioSSena <fabiossena@gmail.com>
2019-03-14 15:31:27 -0300

    teste erro constraint

--- src/main/java/com/packageIxia/sistemaControleEscala/models/projeto/ProjetoEscalaPrestador.java
+++ src/main/java/com/packageIxia/sistemaControleEscala/models/projeto/ProjetoEscalaPrestador.java
@@ -86,7 +86,7 @@ public class ProjetoEscalaPrestador {
 	private LocalDateTime dataExcluido;
 	
     @OneToMany(fetch = FetchType.LAZY, mappedBy="projetoEscalaPrestador", cascade = CascadeType.ALL, orphanRemoval = true)
-    transient private List<ProjetoEscalaPrestadorDiaHoraTrabalho> projetoEscalaPrestadorDiaHoraTrabalhos = new ArrayList<ProjetoEscalaPrestadorDiaHoraTrabalho>();
+    private List<ProjetoEscalaPrestadorDiaHoraTrabalho> projetoEscalaPrestadorDiaHoraTrabalhos = new ArrayList<ProjetoEscalaPrestadorDiaHoraTrabalho>();
     
 	// todo: ajustar
     transient private List<ProjetoFolgaSemanal> projetoFolgasSemanais;
